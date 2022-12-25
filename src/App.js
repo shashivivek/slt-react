9af611bd18e92c247b74cbf13aefed669996d20b
vivek kumar <vivekkumar@imac.mynetworksettings.com>
2022-12-24 23:19:07 -0500

    header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Typed from "typed.js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
 
     <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+
+    <div className="main-info">
+        <h1>Affordable Cloud, Web and Digital Marketing
+Services</h1>
+        <h3>TOP-RATED WEB AND MOBILE APP DEVELOPMENT COMPANY</h3>
+        <h4>Diginto Foundation delivers the most affordable Cloud, Web and Digital Marketing
+services <br />with highly qualified professionals and making businesses
+profitable as well as <br />uptime for their valuable users.</h4><br />
+        <form className="d-flex">
+        <button class="btn btn-outline-warning" type="submit">Learn more</button>
+        <button class="btn btn-warning "  type="submit">Hire us</button>
+        </form>
+    </div>
+
+    </div>
+  )
+}
+
+export default Header;
